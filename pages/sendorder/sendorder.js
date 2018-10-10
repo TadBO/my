4994364b53f50610f674c2061e11708ac770fea6
wangxu321 <353188971@qq.com>
2018-10-10 23:08:49 +0800

    报价页，发布页

---
+++ pages/sendorder/sendorder.js
@@ -0,0 +1,89 @@
+// pages/sendorder/sendorder.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    countries: ["请选择", "美国", "英国"],
+    countryIndex: 0,
+    address:{},
+    cityName:"3241234"
+  },
+  bindCountryChange: function (e) {
+    console.log('picker country 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      countryIndex: e.detail.value
+    })
+  },
+  chooseAddress: function (e) {
+    var that = this;
+    console.log("获取用户地址");
+    wx.chooseAddress({
+      success: function (res) {
+        console.log(res);
+        that.address=res;
+        console.log(that.cityName);
+        that.setData({
+          cityName: res.cityName
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
